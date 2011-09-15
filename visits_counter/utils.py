75e24d0df21b2fdc83ff3123153c76da9e0fb9e5
Jesus Espino Garcia <jmespino@entel.es>
2011-09-15 19:44:48 +0200

    Fixed bug #1: Patch from antycjon applied

--- visits_counter/utils.py
+++ visits_counter/utils.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 def add_visit(request,obj):
     visit = Visit()
-    visit.ip_address = request.META['REMOTE_ADDR']
-    visit.user_agent = request.META['HTTP_USER_AGENT']
+    visit.ip_address = request.META.get('REMOTE_ADDR', '')
+    visit.user_agent = request.META.get('HTTP_USER_AGENT', '')
     visit.object_id = obj.id
     visit.object_model = obj.__class__.__name__
     visit.time = datetime.today()
